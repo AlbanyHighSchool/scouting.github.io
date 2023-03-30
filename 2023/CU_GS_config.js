51b9fdc528372545271208bd6dd9455dda73b3b9
landaj16 <46354577+landaj16@users.noreply.github.com>
2023-03-30 19:53:35 -0400

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023nytr",
+      "defaultValue": "2023NYTR",
       "required": "true",
       "disabled": "true"
     },
